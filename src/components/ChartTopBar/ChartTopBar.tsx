e7c7aff17b24ed1abafbe9b1e6cf831056bfda38
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2020-05-07 04:14:35 +0530

    Fixes chat top bar items

--- src/components/ChartTopBar/ChartTopBar.tsx
+++ src/components/ChartTopBar/ChartTopBar.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./ChartTopBar.scss";
 import ToolboxItem from "../ToolboxItem";
-import { Aave, Uniswap } from "../../services/BlocksService";
+import {
+  Aave,
+  Uniswap,
+  End,
+  UniswapAddLiquidity,
+} from "../../services/BlocksService";
+import { TOKEN_LIST } from "../../constants";
+import { ethers } from "ethers";
 
 function ChartTopBar() {
   const aave = Aave();
   const uniswap = Uniswap();
+  const uniswapAddLiquidity = UniswapAddLiquidity();
+  const end = End();
 
   return (
     <div className="chart-top-bar">
@@ -16,11 +25,18 @@ function ChartTopBar() {
             id: "port1",
             type: "bottom",
             properties: {
-              custom: "property",
+              type: "reserve",
+              amount: 0,
+              asset: TOKEN_LIST[0],
             },
           },
         }}
-        properties={aave}
+        properties={{
+          ...aave,
+          name: "Aave:Flash Loan",
+          type: "initial",
+          nodeType: "flashLoan",
+        }}
       />
       <ToolboxItem
         type="Uniswap:Swap"
@@ -29,18 +45,34 @@ function ChartTopBar() {
             id: "port1",
             type: "top",
             properties: {
-              custom: "property",
+              type: "input",
+              amount: 0,
+              asset: TOKEN_LIST[0],
             },
           },
           port2: {
             id: "port2",
             type: "bottom",
             properties: {
-              custom: "property",
+              type: "output",
+              amount: 0,
+              asset: TOKEN_LIST[0],
             },
           },
         }}
-        properties={uniswap}
+        properties={{
+          ...uniswap,
+          name: "Uniswap:Swap",
+          nodeType: "swap",
+          amountIn: ethers.utils.parseUnits("10", "ether"),
+          amountOutMin: ethers.utils.parseUnits("9.9", "ether"),
+          path: [
+            "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd",
+            "0xaaf64bfcc32d0f15873a02163e7e500671a4ffcd",
+          ],
+          to: "0x038AD9777dC231274553ff927CcB0Fd21Cd42fb9",
+          deadline: 1590969600,
+        }}
       />
       <ToolboxItem
         type="Uniswap:Add Liquidity"
@@ -49,20 +81,31 @@ function ChartTopBar() {
             id: "port1",
             type: "top",
             properties: {
-              custom: "property",
+              type: "input",
+              amount: 0,
+              asset: TOKEN_LIST[0],
             },
           },
           port2: {
             id: "port2",
             type: "top",
             properties: {
-              custom: "property",
+              type: "input",
+              amount: 0,
+              asset: TOKEN_LIST[1],
             },
           },
         }}
         properties={{
-          name: "Uniswap:Add Liquidity",
-          type: "initial",
+          ...uniswapAddLiquidity,
+          tokenA: TOKEN_LIST[0],
+          tokenB: TOKEN_LIST[1],
+          amountADesired: 0,
+          amountBDesired: 0,
+          amountAMin: 0,
+          amountBMin: 0,
+          to: "0x038AD9777dC231274553ff927CcB0Fd21Cd42fb9",
+          deadline: 1590969600,
         }}
       />
       <ToolboxItem
@@ -72,13 +115,14 @@ function ChartTopBar() {
             id: "port1",
             type: "top",
             properties: {
-              custom: "property",
+              type: "input",
+              amount: 0,
+              asset: TOKEN_LIST[0],
             },
           },
         }}
         properties={{
-          name: "End",
-          type: "initial",
+          ...end,
         }}
       />
     </div>
--- src/services/BlocksService.ts
+++ src/services/BlocksService.ts
@@ -4,91 +4,103 @@ import { ethers } from "ethers";
 
 /*
  An export for each type of block.
- */ 
+ */
+
 export interface ITransaction {
-    to: string,
-    txData: string,
+  to: string;
+  txData: string;
 }
 
- export interface IBlock {
-     name: string,
-     type: string,
-     codegen?: (_: INode) => (ITransaction);
- }
-
- export const Aave = () : IBlock => {
-     return {
-         name: "Aave",
-         type: "initial",
-         /* codegen: null, // Null as the contract has the logic by itself */
-     }
- }
-
- export const Splitter = () : IBlock => {
-     return {
-         name: "Splitter",
-         type: "splitter",
-         /* codegen: null, // No need */
-     }
- }
-
- export const End = () : IBlock => {
-     return {
-         name: "End",
-         type: "end",
-         /* codegen: null, // No need */
-     }
- }
-
-export const Uniswap = () : IBlock => {
-    return {
-        name: "Uniswap",
-        type: "exchange",
-        codegen: (node: INode) : ITransaction => {
-            const uniswap = new ethers.utils.Interface(UNISWAP_ABI);
-            const txData = uniswap.functions.swapExactTokensForTokens.encode([
-                node.properties.amountIn,
-                node.properties.amountOutMin,
-                node.properties.path,
-                node.properties.to,
-                node.properties.deadline]);
-            const txTo = UNISWAP_ADDRESS;
-            return { to: txTo, txData: txData };
-        }
-    }
+export interface IBlock {
+  name: string;
+  type: string;
+  codegen?: (_: INode) => ITransaction;
 }
 
-export const UniswapAddLiquidity = () : IBlock => {
-    return {
-        name: "UniswapAddLiquidity",
-        type: "exchange",
-        codegen: (node: INode) : ITransaction => {
-            const uniswap = new ethers.utils.Interface(UNISWAP_ABI);
-            let txData;
-            if(node.properties.tokenA.symbol == "ETH" || node.properties.tokenB.symbol == "ETH" ){
-                txData = uniswap.functions.addLiquidityEth.encode([
-                    node.properties.tokenA.symbol == "ETH"? node.properties.tokenB.address : node.properties.tokenA.address,
-                    node.properties.tokenA.symbol == "ETH"? node.properties.amountBDesired : node.properties.amountADesired,
-                    node.properties.tokenA.symbol == "ETH"? node.properties.amountBMin : node.properties.amountAMin,
-                    node.properties.tokenA.symbol == "ETH"? node.properties.amountAMin : node.properties.amountBMin,
-                    node.properties.to,
-                    node.properties.deadline]);
-                }
-            else{
-            txData = uniswap.functions.addLiquidity.encode([
-                node.properties.tokenA.address,
-                node.properties.tokenB.address,
-                node.properties.amountADesired,
-                node.properties.amountBDesired,
-                node.properties.amountAMin,
-                node.properties.amountBMin,
-                node.properties.to,
-                node.properties.deadline]);
-            }
-            const txTo = UNISWAP_ADDRESS;
-            return { to: txTo, txData: txData };
-        }
-    }
-}
+export const Aave = (): IBlock => {
+  return {
+    name: "Aave",
+    type: "initial",
+    /* codegen: null, // Null as the contract has the logic by itself */
+  };
+};
 
+export const Splitter = (): IBlock => {
+  return {
+    name: "Splitter",
+    type: "splitter",
+    /* codegen: null, // No need */
+  };
+};
 
+export const End = (): IBlock => {
+  return {
+    name: "End",
+    type: "end",
+    /* codegen: null, // No need */
+  };
+};
+
+export const Uniswap = (): IBlock => {
+  return {
+    name: "Uniswap",
+    type: "exchange",
+    codegen: (node: INode): ITransaction => {
+      const uniswap = new ethers.utils.Interface(UNISWAP_ABI);
+      const txData = uniswap.functions.swapExactTokensForTokens.encode([
+        node.properties.amountIn,
+        node.properties.amountOutMin,
+        node.properties.path,
+        node.properties.to,
+        node.properties.deadline,
+      ]);
+      const txTo = UNISWAP_ADDRESS;
+      return { to: txTo, txData: txData };
+    },
+  };
+};
+
+export const UniswapAddLiquidity = (): IBlock => {
+  return {
+    name: "UniswapAddLiquidity",
+    type: "exchange",
+    codegen: (node: INode): ITransaction => {
+      const uniswap = new ethers.utils.Interface(UNISWAP_ABI);
+      let txData;
+      if (
+        node.properties.tokenA.symbol === "ETH" ||
+        node.properties.tokenB.symbol === "ETH"
+      ) {
+        txData = uniswap.functions.addLiquidityEth.encode([
+          node.properties.tokenA.symbol === "ETH"
+            ? node.properties.tokenB.address
+            : node.properties.tokenA.address,
+          node.properties.tokenA.symbol === "ETH"
+            ? node.properties.amountBDesired
+            : node.properties.amountADesired,
+          node.properties.tokenA.symbol === "ETH"
+            ? node.properties.amountBMin
+            : node.properties.amountAMin,
+          node.properties.tokenA.symbol === "ETH"
+            ? node.properties.amountAMin
+            : node.properties.amountBMin,
+          node.properties.to,
+          node.properties.deadline,
+        ]);
+      } else {
+        txData = uniswap.functions.addLiquidity.encode([
+          node.properties.tokenA.address,
+          node.properties.tokenB.address,
+          node.properties.amountADesired,
+          node.properties.amountBDesired,
+          node.properties.amountAMin,
+          node.properties.amountBMin,
+          node.properties.to,
+          node.properties.deadline,
+        ]);
+      }
+      const txTo = UNISWAP_ADDRESS;
+      return { to: txTo, txData: txData };
+    },
+  };
+};

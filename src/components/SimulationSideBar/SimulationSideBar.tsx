c8056934da87f50de142efdbf367fa7157ab1eea
Vitor Py <vitorpy@gmail.com>
2020-05-10 02:13:18 +0100

    Minimal send transaction, broken

--- src/components/SimulationSideBar/SimulationSideBar.tsx
+++ src/components/SimulationSideBar/SimulationSideBar.tsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./SimulationSideBar.scss";
 import { buildTransaction} from "../../services/SimulationService";
 import { IChart } from "@mrblenny/react-flow-chart";
+import { ethers } from "ethers";
+
+import { FACTORY_ADDRESS, FACTORY_ABI, AAVE_ETHEREUM } from "../../constants";
+import Web3 from "web3";
+import { AsyncSendable } from "ethers/providers";
 
 export interface ISimulationSideBarProps {
   chart: IChart;
@@ -28,6 +33,19 @@ function SimulationSideBar({ chart } : ISimulationSideBarProps) {
     );
   }
 
+  const submitTransaction = () => {
+    const web3 = new Web3();
+    const provider = new ethers.providers.Web3Provider(web3.currentProvider as AsyncSendable);
+    const signer = provider.getSigner();
+
+    const Factory = new ethers.Contract(FACTORY_ADDRESS, FACTORY_ABI, signer);
+    const legs = JSON.parse(state.tx).map((item: any) => {
+      return { to: item.to,
+       input: item.txData,
+       value: 0, }});
+    Factory.execute(AAVE_ETHEREUM, legs).then(() => { console.log('done' )});
+  }
+
   return (
     <div className="simulation-side-bar">
       <div className="simulation-title">Simulation</div>
@@ -35,7 +53,7 @@ function SimulationSideBar({ chart } : ISimulationSideBarProps) {
       <div className="simulation-summary-item">{state.error ? "Error"  : state.tx}</div>
       </div>
       <div className="simulation-button-container">
-        <button type="button" className="simulation-button">
+        <button onClick={submitTransaction} type="button" className="simulation-button" disabled={state.tx ? false : true}>
           Execute Transaction
         </button>
       </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,10 @@
 import UNISWAP from "./abis/IUniswapV2Router01.json";
+import FACTORY from "./abis/ExecutorFactory.json";
 
 export const UNISWAP_ADDRESS: string = "0xcDbE04934d89e97a24BCc07c3562DC8CF17d8167";
 export const UNISWAP_ABI: string = JSON.stringify(UNISWAP);
+
+export const FACTORY_ADDRESS: string = "0x583476481c88f8e08a55688458cA4ea4c19cC870";
+export const FACTORY_ABI: string = JSON.stringify(FACTORY);
+
+export const AAVE_ETHEREUM: string = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";

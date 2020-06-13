881c63bdf722cf7876edbdd623887cfea535786c
Vitor Py <vitorpy@gmail.com>
2020-06-13 17:03:06 +0100

    Initial refactoring

---
+++ src/entities/token.ts
@@ -0,0 +1,25 @@
+import { ChainId } from "../constants";
+
+export class Token {
+  public readonly chainId: ChainId;
+  public readonly address: string;
+  public readonly decimals: number;
+  public readonly symbol?: string;
+  public readonly name?: string;
+
+  constructor(
+    chainId: ChainId,
+    address: string,
+    decimals: number,
+    symbol?: string,
+    name?: string
+  ) {
+    validateSolidityTypeInstance(JSBI.BigInt(decimals), SolidityType.uint8);
+
+    this.chainId = chainId;
+    this.address = validateAndParseAddress(address);
+    this.decimals = decimals;
+    if (typeof symbol === "string") this.symbol = symbol;
+    if (typeof name === "string") this.name = name;
+  }
+}
